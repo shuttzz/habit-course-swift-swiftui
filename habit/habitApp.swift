26e7cbd0501bcaf71a66d177295c9da2f69dcfe9
shuttzz <netomenezesucg@gmail.com>
2023-02-06 20:22:53 -0300

    Initial Commit

---
+++ habit/habitApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitApp.swift
+//  habit
+//
+//  Created by José Assis de Menezes Neto on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct habitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
